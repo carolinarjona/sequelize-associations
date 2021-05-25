d24fc2ab5d003af8987a0d3efcabdf5caae7db63
Carolina Arjona Soler <carolinarjona@gmail.com>
2021-05-25 20:29:38 +0200

    Improvement made

--- repositories/commentRepository.js
+++ repositories/commentRepository.js
@@ -1,13 +1,9 @@
 const Comment = require("../models/Comment");
 const User = require("../models/User");
-const Post = require("../models/Post");
 
 exports.findAllComents = async () => {
   return await Comment.findAll({
-    include: [
-      { model: User, attributes: ["name"] },
-      { model: Post, attributes: ["id"] },
-    ],
+    include: { model: User, attributes: ["name"] },
   });
 };
 
